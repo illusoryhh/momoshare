64c91da861ad23d5efb4ba91db34f2bdb2b75a26
illusoryhh <140376241+illusoryhh@users.noreply.github.com>
2023-10-21 07:48:48 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/page?sid=c00d7980b35438413756f841f4fcc98c&uid=26420118&pid=a1878ee730e0a450b14dbd7344206229&tid=529cdaa94fe26daa87ed4efff81bc7ac&view_time=1697774229&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=df4a48ebb9030a46f57f99abd724a5f90b1482ebd0b86226a612405e86c04648&expired_time=2024-10-19T11:57:09%2008:00",
-    "https://www.maimemo.com/page?sid=c00d7980b35438413756f841f4fcc98c&uid=26420118&pid=a1878ee730e0a450b14dbd7344206229&tid=529cdaa94fe26daa87ed4efff81bc7ac&view_time=1697774229&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=df4a48ebb9030a46f57f99abd724a5f90b1482ebd0b86226a612405e86c04648&expired_time=2024-10-19T11:57:09%2008:00",
-    "https://www.maimemo.com/page?sid=c00d7980b35438413756f841f4fcc98c&uid=26420118&pid=a1878ee730e0a450b14dbd7344206229&tid=529cdaa94fe26daa87ed4efff81bc7ac&view_time=1697774229&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=df4a48ebb9030a46f57f99abd724a5f90b1482ebd0b86226a612405e86c04648&expired_time=2024-10-19T11:57:09%2008:00",
+    "https://www.maimemo.com/share/page?uid=26420118&pid=99d1c2b656596efa4308a3f28eb2ff61&tid=14637fe8188b12f649e3e580cafa5936",
+    "https://www.maimemo.com/share/page?uid=26420118&pid=99d1c2b656596efa4308a3f28eb2ff61&tid=14637fe8188b12f649e3e580cafa5936",
+    "https://www.maimemo.com/share/page?uid=26420118&pid=99d1c2b656596efa4308a3f28eb2ff61&tid=14637fe8188b12f649e3e580cafa5936",
   ]
   /**
    * 生成随机数字
