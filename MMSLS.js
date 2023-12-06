e7fcb91a32bb739035a544bef1af65b73c33ae9f
illusoryhh <140376241+illusoryhh@users.noreply.github.com>
2023-12-06 11:04:35 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/page?sid=2fcf683f6ab845b2f379ea4caf26ca3a&uid=26420118&pid=4ae1160f59431ccc747c36b6479beeb4&tid=46c74cf68e9380432d6015483bf79298&view_time=1701245223&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=4cb9695d65b946f96c3eff268fe522e80b73fc5c491e0634d85eeb62b234f55f&expired_time=2024-11-28T16:07:02%2008:00",
-    "https://www.maimemo.com/page?sid=2fcf683f6ab845b2f379ea4caf26ca3a&uid=26420118&pid=4ae1160f59431ccc747c36b6479beeb4&tid=46c74cf68e9380432d6015483bf79298&view_time=1701245223&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=4cb9695d65b946f96c3eff268fe522e80b73fc5c491e0634d85eeb62b234f55f&expired_time=2024-11-28T16:07:02%2008:00",
-    "https://www.maimemo.com/page?sid=2fcf683f6ab845b2f379ea4caf26ca3a&uid=26420118&pid=4ae1160f59431ccc747c36b6479beeb4&tid=46c74cf68e9380432d6015483bf79298&view_time=1701245223&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=4cb9695d65b946f96c3eff268fe522e80b73fc5c491e0634d85eeb62b234f55f&expired_time=2024-11-28T16:07:02%2008:00",
+    "https://www.maimemo.com/page?sid=d82c3ec5899bcd28e185149d5b5c8cfd&uid=26420118&pid=ca61e29648691a4736dab4b5837e55a4&tid=921fe97127078706bdf6bfd6b8462e42&view_time=1701831800&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=d6155082a68f3f3738abebb182751f51eb96f59f5e206aeebf85dc9ebbfbd689&expired_time=2024-12-05T11:03:20%2008:00",
+    "https://www.maimemo.com/page?sid=d82c3ec5899bcd28e185149d5b5c8cfd&uid=26420118&pid=ca61e29648691a4736dab4b5837e55a4&tid=921fe97127078706bdf6bfd6b8462e42&view_time=1701831800&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=d6155082a68f3f3738abebb182751f51eb96f59f5e206aeebf85dc9ebbfbd689&expired_time=2024-12-05T11:03:20%2008:00",
+    "https://www.maimemo.com/page?sid=d82c3ec5899bcd28e185149d5b5c8cfd&uid=26420118&pid=ca61e29648691a4736dab4b5837e55a4&tid=921fe97127078706bdf6bfd6b8462e42&view_time=1701831800&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit5QzW0mfQewOMijwGr74A9M&token=d6155082a68f3f3738abebb182751f51eb96f59f5e206aeebf85dc9ebbfbd689&expired_time=2024-12-05T11:03:20%2008:00",
   ]
   /**
    * 生成随机数字
